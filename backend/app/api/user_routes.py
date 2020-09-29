6527a60a12f02ee1bcf528ac397c3895631f7ace
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-09-28 23:01:37 -0500

    added user/signup user/login backend routes

--- backend/app/api/user_routes.py
+++ backend/app/api/user_routes.py
@@ -1,23 +1,42 @@
 from flask import Blueprint, jsonify, redirect, url_for
 from app.models import User
 from flask_login import current_user, login_user, logout_user,login_required
+from flask_jwt_extended import create_access_token, jwt_required
 
 
 user_routes = Blueprint('users', __name__)
-routes = Blueprint('routes', __name__)
 
-@user_routes.route('/')
-def index():
-  response = User.query.all()
-  return { "users": [user.to_dict() for user in response]}
+@user_routes.route('/login', methods=['POST'])
+def login_user():
+  email = request.json.get('email', None)
+  if not email:
+    return jsonify("msg": "Please enter email."), 400
 
-@routes.route("/")
-def index():
-    return render_template("home.html")
+  password = request.json.get('password', None)
+  if not password:
+    return jsonify("msg": "Please enter password."), 400
 
-@routes.route("/projects/<int:id>")
-def pledge(id):
-    if current_user.is_authenticated:
-        return render_template("pledge.html")
-    else:
-        return redirect(url_for(".login"))
+  user = User.query.filter(User.email=email).one()
+  if(user.check_password(password)):
+    access_token = create_access_token(identity=email)
+    return {"token": access_token, "user": user.to_dict()}, 200
+  else: 
+    return jsonify("msg": "Incorrect email or password."), 400
+    
+@user_routes.route('/signup', methods=['POST'])
+def signup_user():
+  try:
+    data = request.json.get()
+    
+    user = User(
+        email=data['email']
+        password=data['password'] # Maybe a problem?
+        username=data['username']
+    )
+    db.session.add(user)
+    db.session.commit()
+    email= user.email
+    access_token = create_access_token(identity=email)
+    return {"token": access_token, "user": user.to_dict()}, 200
+  except:
+    return jsonify{"msg": "Bad data for signup."}, 400

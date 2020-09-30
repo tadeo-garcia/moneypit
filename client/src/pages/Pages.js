7f5a53a7919596bd08de11907ba82448d94feb30
Brandon Held <wildwestcowboy12@gmail.com>
2020-09-29 19:24:13 -0500

    merges

--- client/src/pages/Pages.js
+++ client/src/pages/Pages.js
@@ -1,8 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
 import Navbar from '../components/Navbar';
->>>>>>> c95ff6c31c56fdb58a2d346cbad8187134eaefe2
 import Auth from '../components/Auth'
 
 

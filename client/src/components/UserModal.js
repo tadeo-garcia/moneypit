d2272b36fd3bacbdae9c1115f60f32e0067d12de
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-10-01 22:42:42 -0500

    working  on scroll bar in user modal

--- client/src/components/UserModal.js
+++ client/src/components/UserModal.js
@@ -2,7 +2,6 @@ import React, {useEffect} from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../store/auth'
-// import '../css/navbar.css';
 import '../css/usermodal.css'
 import { getProjectsByOwner, getProject, getProjectsByPledge } from '../store/project'
 
@@ -42,16 +41,17 @@ const projects_pledged = useSelector(state => state.projects.projectsPledge)
 
   return (
     <div>
+      <div onClick={hideModal} id='hide-modal'></div>
       <div id="user-modal">
         <div id='user-modal-top'>
-          <div className='user-column user-column1'>
-            <h3>YOUR ACCOUNT</h3>
+          <div className='user-column1'>
+            <h3 id='usermodal__header'>YOUR ACCOUNT</h3>
             <Link to='/profile' id='user-modal__link'>Profile</Link>
             <Link to='/settings' id='user-modal__link'>Settings</Link>
             <Link to='/messages' id='user-modal__link'>Messages</Link>
           </div>
-          <div className='user-column user-column2'>
-            <h3>BACKED PROJECTS</h3>
+          <div className='user-column2'>
+            <div><h3 id='usermodal__header'>BACKED PROJECTS</h3></div>
             {projects_pledged.map((project,index)=>{
               let link = `/projects/${project.id}`
               return (
@@ -70,23 +70,25 @@ const projects_pledged = useSelector(state => state.projects.projectsPledge)
               )
             })}
           </div>
-          <div className='user-column'>
-            <h3>CREATED PROJECTS</h3>
-            {projects_owned.map((project,index)=>{
-              let link = `/projects/${project.id}`
-              return (
-                <div id= 'usermodal__project'>
-                  <img src={project.pic} id='backed-modal-pic' alt='project'/>
-                  <a href={link} id={project.id} 
-                  key={project.id} onClick={searchID}
-                  id='user-modal__link'> 
-                  {project.title}
-                  </a>
-                </div>
-              )
-            })}
+          <div className='user-column3'>
+            <div><h3 id='usermodal__header'>CREATED PROJECTS</h3></div>
+            <div class='scroll'>
+              {projects_owned.map((project,index)=>{
+                let link = `/projects/${project.id}`
+                return (
+                  <div id= 'usermodal__project'>
+                    <img src={project.pic} id='backed-modal-pic' alt='project'/>
+                    <a href={link} id={project.id} 
+                    key={project.id} onClick={searchID}
+                    id='user-modal__link'> 
+                    {project.title}
+                    </a>
+                  </div>
+                )
+              })}
+            </div>
             <div id='user-modal__button'>
-              <button id="new-project-button">+ New</button>
+              <button id="new-project-button"> +  New</button>
             </div>
           </div>
         </div>

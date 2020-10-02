725b143050c4923421c42b5bd135b9941f41e735
Jesus Tadeo Garcia <tadeoalyta@gmail.com>
2020-10-02 11:13:15 -0500

    added to the readme heroku instructions

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from "react-router-dom";
-import { getProject } from '../store/project';
 import SearchModal from './SearchModal'
 
 export default function SearchBar({ hideModal }) {
   const [searchTerm, setSearchTerm] = useState('')
   const dispatch = useDispatch();
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   dispatch(getProject(id))
-  //   history.push(`/listings/${id}`)
-  // }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // dispatch(searchCity(city,state))
+    // history.push(`/listings/${city}`)
+  }
 
   const closeSearch = () => {
     setSearchTerm('')
@@ -20,10 +19,10 @@ export default function SearchBar({ hideModal }) {
 
   return (
     <div id='search-div'>
-      <div className="flex-form" onChange={e => setSearchTerm(e.target.value)}>
-        <input type="text" id="search-input" placeholder="Search for Projects or Categories" />
+      <div className="flex-form" onSubmit={handleSubmit} onChange={e => setSearchTerm(e.target.value)}>
+        <input type="text" id="search-input" autoComplete='off' placeholder="Search for Projects or Categories" />
         {(searchTerm ? <i onClick={closeSearch} className='fa fa-times close_query' id='close-query-button' /> : null)}
-        <SearchModal searchTerm={searchTerm} hideModal={hideModal} />
+        <SearchModal searchTerm={searchTerm} />
         {/* <button onClick={hideModal} id='close-search-button' > */}
         <i onClick={hideModal} class='fa fa-times close_search' id='close-search-button' >
         </i>
